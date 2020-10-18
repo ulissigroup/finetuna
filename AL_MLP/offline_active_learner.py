86a7e8076837cb203d5004aaa821a04372b8b719
Rui Qi Chen <ruiqic@andrew.cmu.edu>
2020-10-17 21:48:13 -0400

    updated offlineAL to work with all trainers

--- AL_MLP/offline_active_learner.py
+++ AL_MLP/offline_active_learner.py
@@ -1,7 +1,9 @@
-from amptorch.ase_utils import AMPtorch
-from amptorch.trainer import AtomsTrainer
+import random
+#from amptorch.ase_utils import AMPtorch
+#from amptorch.trainer import AtomsTrainer
 from calcs import DeltaCalc
 from utils import convert_to_singlepoint, compute_with_calc
+
 
 class OfflineActiveLearner:
     """Offline Active Learner
@@ -11,30 +13,42 @@ class OfflineActiveLearner:
 
     learner_settings: dict
         Dictionary of learner parameters and settings.
-
-    trainer_config: dict
-        Dictionary of model settings including the first set training data. 
-        The training data must be calculated using the parent_calc.
+        
+    trainer: object
+        An isntance of a trainer that has a train and predict method.
+        
+    training_data: list
+        A list of ase.Atoms objects that have attached calculators.
+        Used as the first set of training data.
 
     parent_calc: ase Calculator object
         Calculator used for querying training data.
         
-    base_calc: ase Calculator object.
-        Calculator used to calculate delta data for training
+    base_calc: ase Calculator object
+        Calculator used to calculate delta data for training.
+        
+    trainer_calc: uninitialized ase Calculator object
+        The trainer_calc should produce an ase Calculator instance
+        capable of force and energy calculations via trainer_calc(trainer)
+        
      """
     
-    def __init__(self, learner_settings, trainer_config, parent_calc, base_calc):
+    def __init__(self, learner_settings, trainer, training_data, parent_calc, base_calc, trainer_calc):
         self.learner_settings = learner_settings
-        self.terminator_func = learner_settings["terminator_func"]
-        self.query_func = learner_settings["query_func"]
-        self.trainer_config = trainer_config
+        self.trainer = trainer
+        self.training_data = training_data
         self.parent_calc = parent_calc
         self.base_calc = base_calc
+        self.trainer_calc_func = trainer_calc
         self.calcs = [parent_calc, base_calc]
         self.init_training_data()
         
     def init_training_data(self):
-        raw_data = self.trainer_config["dataset"]["raw_data"]
+        """
+        Prepare the training data by attaching delta values for training.
+        """
+        
+        raw_data = self.training_data
         sp_raw_data = convert_to_singlepoint(raw_data)
         parent_ref_image = sp_raw_data[0].copy()
         base_ref_image = compute_with_calc(sp_raw_data[:1],self.base_calc)[0]
@@ -58,17 +72,15 @@ class OfflineActiveLearner:
         while not terminate:
             if self.iterations > 0:
                 self.query_data(sample_candidates)
-            self.trainer_config["dataset"]["raw_data"] = self.training_data
                 
-            trainer = AtomsTrainer(config)
-            trainer.train()
-            trainer_calc = AMPtorch(trainer)
+            self.trainer.train(self.training_data)
+            trainer_calc = self.trainer_calc_func(self.trainer)
             trained_calc = DeltaCalc([trainer_calc, self.base_calc], "add", self.refs)
             
             atomistic_method.run(calc=trained_calc, filename="relax")
             sample_candidates = atomistic_method.get_trajectory(filename="relax")
             
-            terminate = self.terminator_func(self)
+            terminate = self.check_terminate()
             self.iterations += 1
             
         return trained_calc
@@ -87,4 +99,19 @@ class OfflineActiveLearner:
         for image in queried_images:
             image.calc = None
         self.training_data += compute_with_calc(queried_images, self.delta_sub_calc)
+    
+    def check_terminate():
+        """
+        Default termination function. Teminates after 10 iterations
+        """
+        if self.iterations >= 10:
+            return True
+        return False
+        
+    def query_func(sample_candidates):
+        """
+        Detault query strategy. Randomly queries 1 data point.
+        """
+        queried_images = random.sample(sample_candidates,1)
+        return queried_images
         
