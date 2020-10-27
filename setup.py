477bded77cca2262fe95d43a5e7071ef4fad810f
Rui Qi Chen <ruiqic@andrew.cmu.edu>
2020-10-27 04:38:31 +0000

    added setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+setup(name='al_mlp',
+      version='0.1',
+      description='Active Learning for Machine Learning Potentials',
+      url='https://github.com/ulissigroup/al_mlp',
+      author='Rui Qi Chen, Matt Adams',
+      author_email='ruiqic@andrew.cmu.edu, madams2@andrew.cmu.edu',
+      packages=find_packages(),
+      include_package_data=False,
+      install_requires=['numpy', 'ase'],
+      long_description='''Module for performing delta active learning \
+                          with machine learning potentials.''',)
