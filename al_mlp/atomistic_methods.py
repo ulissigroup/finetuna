ccb9007e7b506cb102d7757268130ed0bcc5926d
Rui Qi Chen <ruiqic@andrew.cmu.edu>
2021-01-08 14:17:53 +0000

    MD atomistic method

--- al_mlp/atomistic_methods.py
+++ al_mlp/atomistic_methods.py
@@ -1,4 +1,68 @@
 import ase
+import ase.io
+from ase import units
+from ase.md.velocitydistribution import MaxwellBoltzmannDistribution
+from ase.md import VelocityVerlet, Langevin, nvtberendsen
+
+import numpy as np
+
+
+class MDsimulate:
+    def __init__(self, thermo_ensemble, dt, temp, count, initial_geometry=None):
+        """
+        Parameters
+        ----------
+        ensemble: "NVE", "nvtberendsen", "langevin"
+        dt: md time step (fs)
+        temp: temperature (K)
+        initial_slab: initial geometry to use, if None - will be generated
+        """
+        self.ensemble = thermo_ensemble
+        self.dt = dt
+        self.temp = temp
+        self.count = count
+        if initial_geometry is None:
+            raise Exception("Initial structure not provided!")
+        else:
+            self.starting_geometry = initial_geometry
+
+    def run(self, calc, filename):
+        slab = self.starting_geometry.copy()
+        slab.set_calculator(calc)
+        np.random.seed(1)
+        MaxwellBoltzmannDistribution(slab, self.temp * units.kB)
+        if self.ensemble == "NVE":
+            dyn = VelocityVerlet(slab, self.dt * units.fs)
+        elif self.ensemble == "nvtberendsen":
+            dyn = nvtberendsen.NVTBerendsen(
+                slab, self.dt * units.fs, self.temp, taut=300 * units.fs
+            )
+        elif self.ensemble == "langevin":
+            dyn = Langevin(slab, self.dt * units.fs, self.temp * units.kB, 0.002)
+        traj = ase.io.Trajectory(filename + ".traj", "w", slab)
+        dyn.attach(traj.write, interval=1)
+        try:
+            fixed_atoms = len(slab.constraints[0].get_indices())
+        except:
+            fixed_atoms = 0
+            pass
+
+        def printenergy(a=slab):
+            """Function to print( the potential, kinetic, and total energy)"""
+            epot = a.get_potential_energy() / len(a)
+            ekin = a.get_kinetic_energy() / (len(a) - fixed_atoms)
+            print(
+                "Energy per atom: Epot = %.3feV Ekin = %.3feV (T=%3.0fK) "
+                "Etot = %.3feV" % (epot, ekin, ekin / (1.5 * units.kB), epot + ekin)
+            )
+
+        if printenergy:
+            dyn.attach(printenergy, interval=10)
+        dyn.run(self.count)
+
+    def get_trajectory(self, filename):
+        trajectory = ase.io.Trajectory(filename + ".traj")
+        return trajectory
 
 
 class Relaxation:
