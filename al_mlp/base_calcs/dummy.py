740371988978e5c4963be38f1fbe382ab2c3341a
jmusiel <73840022+jmusiel@users.noreply.github.com>
2021-01-26 17:40:34 +0000

    Added dummy calc to base calcs

---
+++ al_mlp/base_calcs/dummy.py
@@ -0,0 +1,18 @@
+from ase.calculators.calculator import Calculator, all_changes
+import numpy as np
+
+class Dummy(Calculator):
+    implemented_properties = ["energy", "forces"]
+
+    def __init__(self, images, **kwargs):
+        Calculator.__init__(self, **kwargs)
+        self.images = images
+
+    def calculate(self, atoms=None, properties=["energy"], system_changes=all_changes):
+        Calculator.calculate(self, atoms, properties, system_changes)
+        image = atoms
+        natoms = len(image)
+        energy = 0.0
+        forces = np.zeros((natoms, 3))
+        self.results["energy"] = energy
+        self.results["forces"] = forces
