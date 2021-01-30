45bfd32c7e8be62552a688d3990f88374c193306
zack ulissi <zulissi@gmail.com>
2021-01-30 11:49:45 -0500

    add test script

---
+++ al_mlp/tests/test_script.py
@@ -0,0 +1,19 @@
+"""
+Test script to be executed before pushing or submitting a PR to master
+repository.
+"""
+
+import unittest
+
+from .delta_energy_test import test_delta_sub, test_delta_add
+
+
+class TestMethods(unittest.TestCase):
+    def test_delta_sub(self):
+        test_delta_sub()
+
+    def test_delta_add(self):
+        test_delta_add()
+
+if __name__ == "__main__":
+    unittest.main(warnings="ignore")
