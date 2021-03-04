85408e1b91468f4a2526a8f2aaca6b85e531097e
jmusiel <73840022+jmusiel@users.noreply.github.com>
2021-03-04 15:49:36 +0000

    sort of fixed online example 2

--- examples/online_al_example_2.py
+++ examples/online_al_example_2.py
@@ -19,97 +19,96 @@ if __name__ == "__main__":
     client = Client(cluster)
     EnsembleCalc.set_executor(client)
 
-# Set up parent calculator and image environment
-initial_structure = Icosahedron("Cu", 2)
-initial_structure.rattle(0.1)
-initial_structure.set_pbc(True)
-initial_structure.set_cell([20, 20, 20])
-images = []
-elements = ["Cu"]
-parent_calc = EMT()
+    # Set up parent calculator and image environment
+    initial_structure = Icosahedron("Cu", 2)
+    initial_structure.rattle(0.1)
+    initial_structure.set_pbc(True)
+    initial_structure.set_cell([20, 20, 20])
+    images = []
+    elements = ["Cu"]
+    parent_calc = EMT()
 
-# Run relaxation with active learning
-OAL_initial_structure = initial_structure.copy()
-OAL_initial_structure.set_calculator(copy.deepcopy(parent_calc))
-OAL_relaxation = Relaxation(
-    OAL_initial_structure, BFGS, fmax=0.05, steps=60, maxstep=0.04
-)
+    # Run relaxation with active learning
+    OAL_initial_structure = initial_structure.copy()
+    OAL_initial_structure.set_calculator(copy.deepcopy(parent_calc))
+    OAL_relaxation = Relaxation(
+        OAL_initial_structure, BFGS, fmax=0.05, steps=60, maxstep=0.04
+    )
 
-
-Gs = {
-    "default": {
-        "G2": {
-            "etas": np.logspace(np.log10(0.05), np.log10(5.0), num=4),
-            "rs_s": [0],
+    Gs = {
+        "default": {
+            "G2": {
+                "etas": np.logspace(np.log10(0.05), np.log10(5.0), num=4),
+                "rs_s": [0],
+            },
+            "G4": {"etas": [0.005], "zetas": [1.0, 4.0], "gammas": [1.0, -1.0]},
+            "cutoff": 6,
         },
-        "G4": {"etas": [0.005], "zetas": [1.0, 4.0], "gammas": [1.0, -1.0]},
-        "cutoff": 6,
-    },
-}
+    }
 
-learner_params = {
-    "max_iterations": 10,
-    "samples_to_retrain": 1,
-    "filename": "relax_example",
-    "file_dir": "./",
-    "uncertain_tol": 5.0,
-    "fmax_verify_threshold": 0.05,  # eV/AA
-    "relative_variance": True,
-    "n_ensembles": 10,
-    "use_dask": True,
-}
+    learner_params = {
+        "max_iterations": 10,
+        "samples_to_retrain": 1,
+        "filename": "relax_example",
+        "file_dir": "./",
+        "uncertain_tol": 5.0,
+        "fmax_verify_threshold": 0.05,  # eV/AA
+        "relative_variance": True,
+        "n_ensembles": 10,
+        "use_dask": True,
+    }
 
-config = {
-    "model": {"get_forces": True, "num_layers": 3, "num_nodes": 5},
-    "optim": {
-        "device": "cpu",
-        "force_coefficient": 4.0,
-        "lr": 1,
-        "batch_size": 10,
-        "epochs": 100,
-        "optimizer": torch.optim.LBFGS,
-        "optimizer_args": {"optimizer__line_search_fn": "strong_wolfe"},
-    },
-    "dataset": {
-        "raw_data": images,
-        "val_split": 0,
-        "elements": elements,
-        "fp_params": Gs,
-        "save_fps": False,
-        "scaling": {"type": "standardize"},
-    },
-    "cmd": {
-        "debug": False,
-        "run_dir": "./",
-        "seed": 1,
-        "identifier": "test",
-        "verbose": False,
-        # "logger": True,
-        "single-threaded": True,
-    },
-}
+    config = {
+        "model": {"get_forces": True, "num_layers": 3, "num_nodes": 5},
+        "optim": {
+            "device": "cpu",
+            "force_coefficient": 4.0,
+            "lr": 1,
+            "batch_size": 10,
+            "epochs": 100,
+            "optimizer": torch.optim.LBFGS,
+            "optimizer_args": {"optimizer__line_search_fn": "strong_wolfe"},
+        },
+        "dataset": {
+            "raw_data": images,
+            "val_split": 0,
+            "elements": elements,
+            "fp_params": Gs,
+            "save_fps": False,
+            "scaling": {"type": "standardize"},
+        },
+        "cmd": {
+            "debug": False,
+            "run_dir": "./",
+            "seed": 1,
+            "identifier": "test",
+            "verbose": False,
+            # "logger": True,
+            "single-threaded": True,
+        },
+    }
 
-dbname = "CuNP_oal"
-trainer = AtomsTrainer(config)
+    dbname = "CuNP_oal"
+    trainer = AtomsTrainer(config)
 
-onlinecalc = OnlineLearner(
-    learner_params,
-    trainer,
-    images,
-    copy.deepcopy(parent_calc),
-)
+    onlinecalc = OnlineLearner(
+        learner_params,
+        trainer,
+        images,
+        copy.deepcopy(parent_calc),
+    )
 
-if os.path.exists("dft_calls.db"):
-    os.remove("dft_calls.db")
-OAL_relaxation.run(onlinecalc, filename=dbname)
+    if os.path.exists("dft_calls.db"):
+        os.remove("dft_calls.db")
+    OAL_relaxation.run(onlinecalc, filename=dbname)
 
-# Retain and print image of the final structure from the online relaxation
-OAL_image = OAL_relaxation.get_trajectory("CuNP_oal")[-1]
-OAL_image.set_calculator(copy.deepcopy(parent_calc))
-print(
-    "Final Image Results:"
-    + "\nEnergy:\n"
-    + str(OAL_image.get_potential_energy())
-    + "\nForces:\n"
-    + str(OAL_image.get_forces())
-)
+    # Retain and print image of the final structure from the online relaxation
+    OAL_image = OAL_relaxation.get_trajectory("CuNP_oal")[-1]
+    OAL_image.set_calculator(copy.deepcopy(parent_calc))
+    print(
+        "Final Image Results:"
+        + "\nEnergy:\n"
+        + str(OAL_image.get_potential_energy())
+        + "\nForces:\n"
+        + str(OAL_image.get_forces())
+    )
