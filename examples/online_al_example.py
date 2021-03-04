bf612fef37d10199bd70ecc39d657ee79defc58f
jmusiel <73840022+jmusiel@users.noreply.github.com>
2021-03-04 15:52:24 +0000

    deleted deprecated online al example and replaced with working one

--- examples/online_al_example.py
+++ examples/online_al_example.py
@@ -1,110 +1,114 @@
-import numpy as np
-import copy
-from al_mlp.online_learner import OnlineActiveLearner
-from ase.calculators.emt import EMT
-from al_mlp.base_calcs.morse import MultiMorse
 from al_mlp.atomistic_methods import Relaxation
-from amptorch.ase_utils import AMPtorch
+from al_mlp.online_learner import OnlineLearner
 from amptorch.trainer import AtomsTrainer
-from ase.build import fcc100, add_adsorbate, molecule
-from ase.constraints import FixAtoms
+from ase.calculators.emt import EMT
+import numpy as np
+from ase.cluster.icosahedron import Icosahedron
 from ase.optimize import BFGS
-from ase.build import bulk
-from ase.utils.eos import EquationOfState
+import torch
+import os
+import copy
 
-parent_calculator = EMT()
-energies = []
-volumes = []
-LC = [3.5, 3.55, 3.6, 3.65, 3.7, 3.75]
+# Set up ensemble parallelization
+if __name__ == "__main__":
+    # import make_ensemble and dask for setting parallelization
+    from al_mlp.ensemble_calc import EnsembleCalc
+    from dask.distributed import Client, LocalCluster
 
-for a in LC:
-    cu_bulk = bulk("Cu", "fcc", a=a)
-    calc = EMT()
-    cu_bulk.set_calculator(calc)
-    e = cu_bulk.get_potential_energy()
-    energies.append(e)
-    volumes.append(cu_bulk.get_volume())
+    cluster = LocalCluster(processes=True, threads_per_worker=1)
+    client = Client(cluster)
+    EnsembleCalc.set_executor(client)
 
+    # Set up parent calculator and image environment
+    initial_structure = Icosahedron("Cu", 2)
+    initial_structure.rattle(0.1)
+    initial_structure.set_pbc(True)
+    initial_structure.set_cell([20, 20, 20])
+    images = []
+    elements = ["Cu"]
+    parent_calc = EMT()
 
-eos = EquationOfState(volumes, energies)
-v0, e0, B = eos.fit()
-aref = 3.6
-vref = bulk("Cu", "fcc", a=aref).get_volume()
-copper_lattice_constant = (v0 / vref) ** (1 / 3) * aref
-slab = fcc100("Cu", a=copper_lattice_constant, size=(2, 2, 3))
-ads = molecule("C")
-add_adsorbate(slab, ads, 2, offset=(1, 1))
-cons = FixAtoms(indices=[atom.index for atom in slab if (atom.tag == 3)])
-slab.set_constraint(cons)
-slab.center(vacuum=13.0, axis=2)
-slab.set_pbc(True)
-slab.wrap(pbc=[True] * 3)
-slab.set_calculator(copy.copy(parent_calculator))
-slab.set_initial_magnetic_moments()
-images = [slab]
+    # Run relaxation with active learning
+    OAL_initial_structure = initial_structure.copy()
+    OAL_initial_structure.set_calculator(copy.deepcopy(parent_calc))
+    OAL_relaxation = Relaxation(
+        OAL_initial_structure, BFGS, fmax=0.05, steps=60, maxstep=0.04
+    )
 
-Gs = {
-    "default": {
-        "G2": {
-            "etas": np.logspace(np.log10(0.05), np.log10(5.0), num=4),
-            "rs_s": [0],
+    Gs = {
+        "default": {
+            "G2": {
+                "etas": np.logspace(np.log10(0.05), np.log10(5.0), num=4),
+                "rs_s": [0],
+            },
+            "G4": {"etas": [0.005], "zetas": [1.0, 4.0], "gammas": [1.0, -1.0]},
+            "cutoff": 6,
         },
-        "G4": {"etas": [0.005], "zetas": [1.0, 4.0], "gammas": [1.0, -1.0]},
-        "cutoff": 6,
-    },
-}
+    }
 
-elements = ["Cu", "C"]
-learner_params = {
-    "max_iterations": 10,
-    "samples_to_retrain": 1,
-    "filename": "relax_example",
-    "file_dir": "./",
-    "uncertain_tol": 0.05,
-}
+    learner_params = {
+        "max_iterations": 10,
+        "samples_to_retrain": 1,
+        "filename": "relax_example",
+        "file_dir": "./",
+        "uncertain_tol": 5.0,
+        "fmax_verify_threshold": 0.05,  # eV/AA
+        "relative_variance": True,
+        "n_ensembles": 10,
+        "use_dask": True,
+    }
 
-config = {
-    "model": {"get_forces": True, "num_layers": 3, "num_nodes": 5},
-    "optim": {
-        "device": "cpu",
-        "force_coefficient": 0.04,
-        "lr": 1e-2,
-        "batch_size": 10,
-        "epochs": 100,
-    },
-    "dataset": {
-        "raw_data": images,
-        "val_split": 0,
-        "elements": elements,
-        "fp_params": Gs,
-        "save_fps": True,
-    },
-    "cmd": {
-        "debug": False,
-        "run_dir": "./",
-        "seed": 1,
-        "identifier": "test",
-        "verbose": True,
-        # "logger": True,
-    },
-}
-cutoff = Gs["default"]["cutoff"]
-parent_calc = EMT()
-trainer = AtomsTrainer(config)
-trainer_calc = AMPtorch
-base_calc = MultiMorse(images, cutoff, combo="mean")
+    config = {
+        "model": {"get_forces": True, "num_layers": 3, "num_nodes": 5},
+        "optim": {
+            "device": "cpu",
+            "force_coefficient": 4.0,
+            "lr": 1,
+            "batch_size": 10,
+            "epochs": 100,
+            "optimizer": torch.optim.LBFGS,
+            "optimizer_args": {"optimizer__line_search_fn": "strong_wolfe"},
+        },
+        "dataset": {
+            "raw_data": images,
+            "val_split": 0,
+            "elements": elements,
+            "fp_params": Gs,
+            "save_fps": False,
+            "scaling": {"type": "standardize"},
+        },
+        "cmd": {
+            "debug": False,
+            "run_dir": "./",
+            "seed": 1,
+            "identifier": "test",
+            "verbose": False,
+            # "logger": True,
+            "single-threaded": True,
+        },
+    }
 
-onlinecalc = OnlineActiveLearner(
-    learner_params,
-    trainer,
-    images,
-    parent_calc,
-    base_calc,
-    trainer_calc,
-    n_ensembles=5,
-    n_cores="max",
-)
+    dbname = "CuNP_oal"
+    trainer = AtomsTrainer(config)
 
-structure_optim = Relaxation(slab, BFGS, fmax=0.05, steps=100)
+    onlinecalc = OnlineLearner(
+        learner_params,
+        trainer,
+        images,
+        copy.deepcopy(parent_calc),
+    )
 
-structure_optim.run(onlinecalc, filename="relax_oal")
+    if os.path.exists("dft_calls.db"):
+        os.remove("dft_calls.db")
+    OAL_relaxation.run(onlinecalc, filename=dbname)
+
+    # Retain and print image of the final structure from the online relaxation
+    OAL_image = OAL_relaxation.get_trajectory("CuNP_oal")[-1]
+    OAL_image.set_calculator(copy.deepcopy(parent_calc))
+    print(
+        "Final Image Results:"
+        + "\nEnergy:\n"
+        + str(OAL_image.get_potential_energy())
+        + "\nForces:\n"
+        + str(OAL_image.get_forces())
+    )
