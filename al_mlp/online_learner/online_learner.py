bf5502f6b1a10cecaed10392de0d369e7d1726b1
Yuri <rsanspeu@andrew.cmu.edu>
2021-04-22 17:47:36 +0000

    Initial attempt at storing metadata on the relaxation steps in a mongo db. Code is very sphagetti-like and I need to find a way not to have to repeat almost the same blocks three times.

--- al_mlp/online_learner/online_learner.py
+++ al_mlp/online_learner/online_learner.py
@@ -2,6 +2,8 @@ import copy
 import numpy as np
 from ase.calculators.calculator import Calculator
 from al_mlp.utils import convert_to_singlepoint
+import pymongo
+import datetime
 
 __author__ = "Muhammed Shuaibi"
 __email__ = "mshuaibi@andrew.cmu.edu"
@@ -17,6 +19,7 @@ class OnlineLearner(Calculator):
         parent_dataset,
         ml_potential,
         parent_calc,
+        task_name,
     ):
         Calculator.__init__(self)
 
@@ -26,9 +29,11 @@ class OnlineLearner(Calculator):
         self.parent_dataset = convert_to_singlepoint(parent_dataset)
 
         self.ml_potential = ml_potential
+        self.task_name = task_name
 
         # Don't bother training with only one data point,
         # as the uncertainty is meaningless
+
         if len(self.parent_dataset) > 1:
             ml_potential.train(self.parent_dataset)
 
@@ -39,16 +44,49 @@ class OnlineLearner(Calculator):
 
         self.uncertain_tol = learner_params["uncertain_tol"]
         self.parent_calls = 0
+        self.iteration = 0
 
     def calculate(self, atoms, properties, system_changes):
         Calculator.calculate(self, atoms, properties, system_changes)
 
+        #breakpoint()
+        # If can make a connection to MongoDB then create a database to house the OAL outputs
+        conn = self.mongodb_conn()
+
+        if conn is not None:
+            # Look to see if the oal_metadata collection exists and if not create it
+            db = conn.get_database('db')
+
+            if 'oal_metadata' not in db.collection_names():
+                db.create_collection('oal_metadata')
+
+            collection = db.get_collection('oal_metadata')
+
         # If we have less than two data points, uncertainty is not
         # well calibrated so just use DFT
+
+
         if len(self.parent_dataset) < 2:
             energy, force = self.add_data_and_retrain(atoms)
             self.results["energy"] = energy
             self.results["forces"] = force
+
+            if collection is not None:
+                self.insert_row(collection, iteration=self.iteration,
+                                            parent_call=True,
+                                            energy=energy,
+                                            force=force.tolist(),
+                                            max_force_stds='NA',
+                                            base_uncertainty='NA',
+                                            uncertainty_tol='NA',
+                                            task_name=self.task_name,
+                                            time_stamp=datetime.datetime.utcnow()
+                                            )
+
+
+            self.iteration += 1
+
+
             return
 
         # Make a copy of the atoms with ensemble energies as a SP
@@ -59,29 +97,63 @@ class OnlineLearner(Calculator):
         #         (atoms_ML,) = convert_to_singlepoint([atoms_copy])
 
         # Check if we are extrapolating too far, and if so add/retrain
+
+
         if self.unsafe_prediction(atoms_ML) or self.parent_verify(atoms_ML):
             # We ran DFT, so just use that energy/force
             energy, force = self.add_data_and_retrain(atoms)
-        else:
-            energy = atoms_ML.get_potential_energy(apply_constraint=False)
-            force = atoms_ML.get_forces(apply_constraint=False)
 
+            if collection is not None:
+                self.insert_row(collection, iteration=self.iteration,
+                                            parent_call=True,
+                                            energy=energy,
+                                            force=force.tolist(),
+                                            max_force_stds=float(self.uncertainty),
+                                            base_uncertainty=float(self.base_uncertainty),
+                                            uncertainty_tol=float(self.uncertainty_tol),
+                                            task_name=self.task_name,
+                                            time_stamp=datetime.datetime.utcnow()
+                                            )
+
+
+        else:
+            if collection is not None:
+                self.insert_row(collection, iteration=self.iteration,
+                                            parent_call=False,
+                                            energy=energy,
+                                            force=force.tolist(),
+                                            max_force_stds=float(self.uncertainty),
+                                            base_uncertainty=float(self.base_uncertainty),
+                                            uncertainty_tol=float(self.uncertainty_tol),
+                                            task_name=self.task_name,
+                                            time_stamp=datetime.datetime.utcnow()
+                                            )
+
+                energy = atoms_ML.get_potential_energy(apply_constraint=False)
+                force = atoms_ML.get_forces(apply_constraint=False)
+
+
+
+
+
+
+        self.iteration += 1
         # Return the energy/force
         self.results["energy"] = energy
         self.results["forces"] = force
 
     def unsafe_prediction(self, atoms):
         # Set the desired tolerance based on the current max predcited force
-        uncertainty = atoms.calc.results["max_force_stds"]
-        base_uncertainty = np.nanmax(np.abs(atoms.get_forces()))
-        uncertainty_tol = self.uncertain_tol * base_uncertainty
+        self.uncertainty = atoms.calc.results["max_force_stds"]
+        self.base_uncertainty = np.nanmax(np.abs(atoms.get_forces()))
+        self.uncertainty_tol = self.uncertain_tol * self.base_uncertainty
 
         print(
             "Max Force Std: %1.3f eV/A, Max Force Threshold: %1.3f eV/A"
-            % (uncertainty, uncertainty_tol)
+            % (self.uncertainty, self.uncertainty_tol)
         )
 
-        if uncertainty > uncertainty_tol:
+        if self.uncertainty > self.uncertainty_tol:
             return True
         else:
             return False
@@ -89,6 +161,7 @@ class OnlineLearner(Calculator):
     def parent_verify(self, atoms):
         forces = atoms.get_forces()
         fmax = np.sqrt((forces ** 2).sum(axis=1).max())
+
         return fmax <= self.fmax_verify_threshold
 
     def add_data_and_retrain(self, atoms):
@@ -105,9 +178,21 @@ class OnlineLearner(Calculator):
         self.parent_dataset += [new_data]
 
         # Don't bother training if we have less than two datapoints
+
         if len(self.parent_dataset) >= 2:
             self.ml_potential.train(self.parent_dataset)
 
         self.parent_calls += 1
 
         return energy_actual, force_actual
+
+    @classmethod
+    def mongodb_conn(cls):
+        """Checks if we can make a connection to a database and if yes returns the connection"""
+        try:
+            return pymongo.MongoClient()
+        except pymongo.errors.ConnectionFailure as e:
+            print(f'Could not connect to server: {e}')
+
+    def insert_row(self, collection,  **kw_args):
+        collection.insert_one(kw_args)
