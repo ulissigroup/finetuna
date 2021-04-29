b75b430f240ca902a1ece65508f0bde265ed861b
LoryWang <xiaoxia3@andrew.cmu.edu>
2021-04-29 00:50:39 +0000

    pass black

--- al_mlp/tests/suites/light_suite.py
+++ al_mlp/tests/suites/light_suite.py
@@ -5,6 +5,7 @@ import unittest
 from al_mlp.tests.cases.case_online_CuNP import online_CuNP
 from al_mlp.tests.cases.case_offline_CuNP import offline_CuNP
 from al_mlp.tests.cases.case_offline_uncertainty_test import offline_uncertainty_CuNP
+
 # from al_mlp.tests.cases.case_online_flare_CuNP import online_flare_CuNP
 
 # import and set executor client
