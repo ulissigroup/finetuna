4f167c746e7456bdd39bc3743aa95d255f75930e
jmusiel <73840022+jmusiel@users.noreply.github.com>
2021-05-12 19:06:56 +0000

    ocp_model memory map size changes

--- al_mlp/base_calcs/ocp_model.py
+++ al_mlp/base_calcs/ocp_model.py
@@ -1,3 +1,4 @@
+from ase.atoms import Atoms
 import yaml
 import os
 import lmdb
@@ -8,6 +9,7 @@ from ocpmodels.preprocessing import AtomsToGraphs
 from ocpmodels.trainers import ForcesTrainer
 from ase.calculators.calculator import Calculator, all_changes
 from torch_geometric.data import Batch
+import copy
 
 from ocpmodels.trainers.forces_trainer import ForcesTrainer
 
@@ -27,6 +29,15 @@ class OCPModel(Calculator):
         **kwargs,
     ):
         Calculator.__init__(self, **kwargs)
+
+        self.model_path = model_path
+        self.checkpoint_path = checkpoint_path
+        self.dataset = dataset
+        self.a2g = a2g
+        self.task = task
+        self.identifier = identifier
+        self.kwargs = kwargs
+
         model_dict = {}
         with open(model_path) as model_yaml:
             model_dict = yaml.safe_load(model_yaml)
@@ -44,7 +55,12 @@ class OCPModel(Calculator):
             }
 
         if not dataset:
-            dataset = [{"src": "s2ef", "normalize_labels": False}]
+            dataset = [
+                {
+                    "src": "/home/jovyan/working/ocp/data/s2ef/2M/train/",
+                    "normalize_labels": False,
+                }
+            ]
 
         if not a2g:
             a2g = AtomsToGraphs(
@@ -70,11 +86,15 @@ class OCPModel(Calculator):
         self.trainer.load_pretrained(checkpoint_path=checkpoint_path)
 
     def calculate(self, atoms=None, properties=["forces"], system_changes=all_changes):
-        super().calculate(
-            atoms=atoms, properties=properties, system_changes=system_changes
+        Calculator.calculate(
+            self, atoms=atoms, properties=properties, system_changes=system_changes
         )
-
-        data_objects = self.ase_to_data(atoms, self.a2g)
+        atoms_list = []
+        if isinstance(atoms, Atoms):
+            atoms_list = [atoms]
+        else:
+            atoms_list = atoms
+        data_objects = self.ase_to_data(atoms_list, self.a2g)
         batch = self.data_to_batch(data_objects)
 
         prediction = self.trainer._forward([batch])
@@ -85,12 +105,23 @@ class OCPModel(Calculator):
         self.results["energy"] = energy
         self.results["forces"] = forces
 
+    def __deepcopy__(self, memo):
+        cls = self.__class__
+        result = cls.__new__(cls)
+        memo[id(self)] = result
+        for k, v in self.__dict__.items():
+            if k is not "trainer":
+                setattr(result, k, copy.deepcopy(v, memo))
+            else:
+                setattr(result, k, v)
+        return result
+
     @staticmethod
     def ase_to_data(ase_images_list, a2g, disable_tqdm=True):
         os.makedirs("s2ef", exist_ok=True)
         db = lmdb.open(
             "s2ef/sample.lmdb",
-            map_size=1099511627776 * 2,
+            map_size=1099511627776/2, # * 2,
             subdir=False,
             meminit=False,
             map_async=True,
