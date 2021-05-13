e723635ffd5f4d02a1e228ad9abf0182d51ba724
Yuri <rsanspeu@andrew.cmu.edu>
2021-05-13 17:15:26 -0500

    yaml.load() deprecated

--- run_firetask.py
+++ run_firetask.py
@@ -21,7 +21,7 @@ def extract_job_parameters(job_id):
     """This function is used to extract the parameter set from the master job_params.yml
     file."""
     with open("job_params.yml", 'r') as stream:
-        hyper_param_set = yaml.load(stream)
+        hyper_param_set = yaml.safe_load(stream)
     print("\nHypermeter set for job_id: ",job_id)
     print("------------------------------------")
     pprint.pprint(hyper_param_set[job_id-1]["hyperparam_set"])
