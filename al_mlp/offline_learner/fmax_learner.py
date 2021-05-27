ba36722c754f19dd14ad3230d2773bab88caed3b
LoryWang <xiaoxia3@andrew.cmu.edu>
2021-05-27 02:42:16 +0000

    change force threshold to pass pytest; minor changes to fmax learner

--- al_mlp/offline_learner/fmax_learner.py
+++ al_mlp/offline_learner/fmax_learner.py
@@ -11,8 +11,12 @@ class FmaxLearner(OfflineActiveLearner):
     Replaces termination criteria with a max force in the constructor and the check_terminate method
     """
 
-    def __init__(self, learner_params, trainer, training_data, parent_calc, base_calc):
-        super().__init__(learner_params, trainer, training_data, parent_calc, base_calc)
+    def __init__(
+        self, learner_params, ml_potential, training_data, parent_calc, base_calc
+    ):
+        super().__init__(
+            learner_params, ml_potential, training_data, parent_calc, base_calc
+        )
         self.max_evA = learner_params["max_evA"]
 
     def check_terminate(self):
@@ -53,7 +57,7 @@ class FmaxLearner(OfflineActiveLearner):
 
     def do_after_train(self):
         """
-        Executes after training the trainer in every active learning loop.
+        Executes after training the ml_potential in every active learning loop.
         """
 
         trainer_calc = self.make_trainer_calc()
