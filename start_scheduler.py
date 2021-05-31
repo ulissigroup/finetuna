dc4878c776e1098aba5b66d3b7cf1cde046a4ccf
Yuri <rsanspeu@andrew.cmu.edu>
2021-05-31 14:09:48 -0400

    Restoring to host agnostic scheduler file for now since I want the specs to be host agnostic for pilot abstractions and being able to rerun fireworks across many hosts. Need to figure out the best way to handle read-only nersc containers.

--- start_scheduler.py
+++ start_scheduler.py
@@ -11,7 +11,6 @@ if __name__ == '__main__':
     loop = IOLoop()
     cluster = LocalCluster(n_workers=workers, threads_per_worker=1, memory_limit=0)
     client = Client(cluster)
-    filename = f"my-scheduler-{os.environ['HOSTNAME']}.json"
-    client.write_scheduler_file(filename)
+    client.write_scheduler_file('my-scheduler.json')
     loop.start()  # keeps the scheduler running
     loop.close()
