b08aadff6f795dccdda542a6bf270fcb9be5ac3d
Yuri <rsanspeu@andrew.cmu.edu>
2021-06-02 17:49:14 -0400

    Make sure VASP binary uses 20 cores and not 8

--- run_fireworks.sh
+++ run_fireworks.sh
@@ -6,7 +6,8 @@
 
 ./run_scheduler.sh & # start the primary process and put it in the background
 
-
+# Use 20 cores to run VASP binary
+export VASP_COMMAND="mpirun -np 20 /opt/vasp.6.1.2_pgi_mkl_beef/bin/vasp_std"
 # Start the second process
 cd .. # launch in PVC /home/jovyan
 rlaunch rapidfire --nlaunches infinite --sleep 10
