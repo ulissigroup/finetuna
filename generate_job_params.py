f0386d50df61e73932f77afabc14c5fe242cd9a6
Yuri <rsanspeu@andrew.cmu.edu>
2021-06-02 21:57:52 -0400

    Sample a wider range and more points

--- generate_job_params.py
+++ generate_job_params.py
@@ -1,7 +1,7 @@
 import yaml
 import numpy as np
 
-uncertain_tols = np.linspace(0.5, 2, 6)
+uncertain_tols = np.linspace(0.5, 15, 15)
 hyperparam_allsets = [dict(param_set=dict(uncertain_tol=float(uncertain_tol))) for uncertain_tol in uncertain_tols]
 
 with open('job_params.yml','w') as outfile:
