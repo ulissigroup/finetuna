017bc48f89bd849b867fceb0c5b09e7e99277623
Yuri <rsanspeu@andrew.cmu.edu>
2021-07-01 23:06:10 -0400

    Lower the perturbations for the rattling.

--- add_to_launchpad.py
+++ add_to_launchpad.py
@@ -25,7 +25,7 @@ if __name__ == "__main__":
 
     structures = [Trajectory('structures/ad_slab.traj')[0].copy() for i in range(10)]
     for i, structure in enumerate(structures):
-        structure.rattle(0.5, seed=np.random.randint(0,high=100000))
+        structure.rattle(0.1, seed=np.random.randint(0,high=100000))
         writer = TrajectoryWriter(filename=f"structures/ad_slab_{i}.traj", mode='w', atoms=structure)
         writer.write()
 
