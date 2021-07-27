65f74d42755ec98e90f72c8f178305846526a2e7
LoryWang <xiaoxia3@andrew.cmu.edu>
2021-07-27 00:55:27 +0000

    only consider the atoms without constraints for PCA; allow different types of input

--- al_mlp/pca.py
+++ al_mlp/pca.py
@@ -7,15 +7,22 @@ import re
 from ase.db import connect
 from sklearn.decomposition import PCA
 import matplotlib.pyplot as plt
+from ase.constraints import constrained_indices
 
 
-def pca(traj_dict):
+def pca(traj_dict, fig_title=None):
     """Perform a PCA analysis for the trajectories.
     Parameters
     ----------
     traj_dict: dict
         Dictionary of calculation types and trajectories.
-        e.g.: {'oal': oal_traj}, where oal_traj is an ASE Trajectory object.
+        e.g.: {'oal': oal_traj}, where oal_traj is an ASE Trajectory
+        object or the path to the trajectory.
+        or {'oal': [path_to_oal_traj, path_to_oal_db]}, where path_to_oal_traj
+        is the path to the trajectory and path_to_oal_db is the path to the ase Database.
+
+    fig_title: str
+        Title of the PCA plot.
     """
     types = []
     trajs = []
@@ -23,8 +30,22 @@ def pca(traj_dict):
     energy = []
     for key, value in traj_dict.items():
         types.append(key)
-        trajs.append(value)
+        if isinstance(value, str):
+            value = Trajectory(value)
+            trajs.append(value)
+        elif isinstance(value, list):
+            traj = Trajectory(value[0])
+            db = connect(value[1])
+            dft_call = [i.id - 1 for i in db.select(check=True)]
+            dft_traj = [traj[i] for i in db_dft_call]
+            trajs.append(dft_traj)
+        else:
+            trajs.append(value)
+    # assuming constraints (FixAtom) are applied to the same atoms
+    constrained_id = constrained_indices(trajs[0][0])
     for i in trajs:
+        all_pos = [j.get_positions() for j in i]
+        free_atom_pos = [np.delete(i, constrained_id, 0) for i in all_pos]
         pos.append([j.get_positions() for j in i])
         energy.append([j.get_potential_energy() for j in i])
     label = []
@@ -58,10 +79,13 @@ def pca(traj_dict):
     ax = fig.add_subplot(1, 1, 1)
     ax.set_xlabel("Principal Component 1", fontsize=15)
     ax.set_ylabel("Principal Component 2", fontsize=15)
-    ax.set_title("2 component PCA", fontsize=20)
+    if fig_title is not None:
+        ax.set_title(fig_title, fontsize=20)
+    else:
+        ax.set_title("Principal Component Analysis", fontsize=20)
     targets = types
     colors = energy
-    mark = ["x", "o", "^"]
+    mark = ["v", "^", "<", ">", "o", "s", "D"]
     for target, color, mark in zip(targets, colors, mark):
         indicesToKeep = finalDf["label"] == target
         #     if target == 'oal':
@@ -71,14 +95,14 @@ def pca(traj_dict):
             c=color,
             marker=mark,
             cmap="winter",
-            s=100,
+            s=50,
+            label=target,
         )
-    #     else:
-    #         ax.scatter(finalDf.loc[indicesToKeep, 'principal component 1']
-    #                    , finalDf.loc[indicesToKeep, 'principal component 2']
-    #                    , c = color
-    #                    , s = 50)
+        ax.plot(
+            finalDf.loc[indicesToKeep, "principal component 1"],
+            finalDf.loc[indicesToKeep, "principal component 2"],
+        )
     sm = plt.cm.ScalarMappable(cmap="winter")
     fig.colorbar(sm)
-    ax.legend(targets)
+    ax.legend()
     plt.savefig("pca.png")
