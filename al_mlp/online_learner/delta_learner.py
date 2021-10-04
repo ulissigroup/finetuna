71bf13012776bd12bc62a3ca5cc9857288669f7e
jmusiel <73840022+jmusiel@users.noreply.github.com>
2021-10-04 23:06:42 +0000

    added delta_learner.py

---
+++ al_mlp/online_learner/delta_learner.py
@@ -0,0 +1,87 @@
+from al_mlp.online_learner.online_learner import OnlineLearner
+from al_mlp.calcs import DeltaCalc
+from al_mlp.utils import convert_to_singlepoint, subtract_deltas
+from al_mlp.mongo import MongoWrapper
+
+
+class DeltaLearner(OnlineLearner):
+    def __init__(
+        self,
+        learner_params,
+        parent_dataset,
+        ml_potential,
+        parent_calc,
+        base_calc,
+        mongo_db=None,
+        optional_config=None,
+    ):
+
+        OnlineLearner.__init__(
+                self,
+                learner_params,
+                parent_dataset,
+                ml_potential,
+                parent_calc,
+                mongo_db=mongo_db,
+                optional_config=optional_config,
+            )
+
+        if mongo_db is not None:
+                self.mongo_wrapper = MongoWrapper(
+                    mongo_db["online_learner"],
+                    learner_params,
+                    ml_potential,
+                    parent_calc,
+                    base_calc,
+                )
+            else:
+                self.mongo_wrapper = None
+
+        self.base_calc = base_calc
+
+        self.parent_ref = compute_with_calc(
+            [initial_structure.copy()], self.parent_calc
+        )[0]
+
+        self.base_ref = compute_with_calc(
+            [initial_structure.copy()], self.base_calc
+        )[0]
+
+        self.refs = [self.parent_ref, self.base_ref]
+
+        self.add_delta_calc = DeltaCalc(
+            [self.ml_potential, self.base_calc],
+            "add",
+            self.refs,
+        )
+
+    def get_ml_prediction(self, atoms_copy):
+        """
+        Helper function which takes an atoms object
+        Makes an Ml prediction.
+        Performs a delta add operation since the ML model was trained on delta sub data.
+        Returns it with a delta ML potential predicted singlepoint.
+        Designed to be overwritten by DeltaLearner which needs to modify ML predictions.
+        """
+        atoms_copy.set_calculator(self.ml_potential)
+        (atoms_with_info,) = convert_to_singlepoint([atoms_copy])
+        atoms_copy.set_calculator(self.add_delta_calc)
+        (atoms_ML,) = convert_to_singlepoint([atoms_copy])
+        for key, value in atoms_with_info.info.items():
+            atoms_ML.info[key] = value
+        return atoms_ML
+
+    def add_to_dataset(self, new_data):
+        """
+        Helper function which takes an atoms object.
+        Performs a delta sub operation on the parent data so that the ML model will train on delta sub data.
+        And adds new parent data to the training set.
+        Returns the partial dataset just added.
+        Designed to overwritten by DeltaLearner which needs to modify data added to training set.
+        """
+        (delta_sub_data,) = subtract_deltas(
+            [new_data], self.base_calc, self.refs
+        )
+        partial_dataset = [delta_sub_data]
+        self.parent_dataset += partial_dataset
+        return partial_dataset
