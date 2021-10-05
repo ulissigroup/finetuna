2889473e06e01c85ab8fabe196b9f9b5d9a1a636
jmusiel <73840022+jmusiel@users.noreply.github.com>
2021-10-04 22:42:32 -0400

    again disable flare unit test for now

--- al_mlp/tests/suites/light_suite.py
+++ al_mlp/tests/suites/light_suite.py
@@ -27,7 +27,7 @@ suite.addTests(loader.loadTestsFromTestCase(offline_CuNP))
 suite.addTests(loader.loadTestsFromModule(offline_uncertainty_CuNP))
 suite.addTests(loader.loadTestsFromModule(delta_CuNP))
 
-suite.addTests(loader.loadTestsFromModule(online_flare_CuNP))
+# suite.addTests(loader.loadTestsFromModule(online_flare_CuNP))
 
 # Deprecated below, call using pytest instead
 # initialize a runner, pass it your suite and run it
