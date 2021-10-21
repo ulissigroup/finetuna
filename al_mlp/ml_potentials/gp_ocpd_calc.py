6b0625b796ee1c356294e57dd58032c555091706
jmusiel <73840022+jmusiel@users.noreply.github.com>
2021-10-21 19:27:14 +0000

    changed gp_ocpd_calc to train on e and f

--- al_mlp/ml_potentials/gp_ocpd_calc.py
+++ al_mlp/ml_potentials/gp_ocpd_calc.py
@@ -1,6 +1,5 @@
 from sklearn.gaussian_process._gpr import GaussianProcessRegressor
 from al_mlp.ml_potentials.ocpd_calc import OCPDCalc
-import numpy as np
 
 
 class GPOCPDCalc(OCPDCalc):
@@ -28,17 +27,21 @@ class GPOCPDCalc(OCPDCalc):
         self.e_model = GaussianProcessRegressor(kernel=self.kernel, **self.mlp_params)
 
     def calculate_ml(self, ocp_descriptor) -> tuple:
-        f_mean, f_std = self.f_model.predict([ocp_descriptor], return_std=True)
-        e_mean, e_std = self.e_model.predict([ocp_descriptor], return_std=True)
+        f_mean, f_std = self.f_model.predict([ocp_descriptor[1]], return_std=True)
+        e_mean, e_std = self.e_model.predict([ocp_descriptor[0]], return_std=True)
         f_mean = f_mean[0].reshape(3, -1).T
         return e_mean[0], f_mean, e_std[0], f_std[0]
 
-    def fit(self, parent_energies, parent_forces, parent_descriptors):
+    def fit(
+        self, parent_energies, parent_forces, parent_e_descriptors, parent_f_descriptors
+    ):
         flattened_parent_forces = []
         for forces in parent_forces:
             flattened_parent_forces.append(forces.flatten())
-        self.f_model.fit(parent_descriptors, flattened_parent_forces)
-        self.e_model.fit(parent_descriptors, parent_energies)
+        self.f_model.fit(parent_f_descriptors, flattened_parent_forces)
+        self.e_model.fit(parent_e_descriptors, parent_energies)
 
-    def partial_fit(self, new_energies, new_forces, new_descriptors):
+    def partial_fit(
+        self, new_energies, new_forces, new_e_descriptors, new_f_descriptors
+    ):
         raise NotImplementedError
