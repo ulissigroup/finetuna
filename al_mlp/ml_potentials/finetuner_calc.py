a52ca544f42f9849ed351bfa4e83f2886b312e13
jmusiel <73840022+jmusiel@users.noreply.github.com>
2021-11-16 20:22:47 +0000

    added separate Trainer class to finetuner_calc.py (unused) to replace OCPCalculator in the future

--- al_mlp/ml_potentials/finetuner_calc.py
+++ al_mlp/ml_potentials/finetuner_calc.py
@@ -10,6 +10,10 @@ from al_mlp.job_creator import merge_dict
 import copy
 import time
 import torch
+from ocpmodels.trainers.forces_trainer import ForcesTrainer
+from ocpmodels.datasets.trajectory_lmdb import data_list_collater
+from ocpmodels.common.utils import setup_imports, setup_logging
+import logging
 
 
 class FinetunerCalc(MLPCalc):
@@ -269,3 +273,90 @@ class GenericDB:
 
     def close_db(self):
         pass
+
+
+class Trainer(ForcesTrainer):
+    def __init__(self, config_yml=None, checkpoint=None, cutoff=6, max_neighbors=50):
+        setup_imports()
+        setup_logging()
+
+        # Either the config path or the checkpoint path needs to be provided
+        assert config_yml or checkpoint is not None
+
+        if config_yml is not None:
+            if isinstance(config_yml, str):
+                config = yaml.safe_load(open(config_yml, "r"))
+
+                if "includes" in config:
+                    for include in config["includes"]:
+                        # Change the path based on absolute path of config_yml
+                        path = os.path.join(config_yml.split("configs")[0], include)
+                        include_config = yaml.safe_load(open(path, "r"))
+                        config.update(include_config)
+            else:
+                config = config_yml
+            # Only keeps the train data that might have normalizer values
+            config["dataset"] = config["dataset"][0]
+        else:
+            # Loads the config from the checkpoint directly
+            config = torch.load(checkpoint, map_location=torch.device("cpu"))["config"]
+
+            # Load the trainer based on the dataset used
+            if config["task"]["dataset"] == "trajectory_lmdb":
+                config["trainer"] = "forces"
+            else:
+                config["trainer"] = "energy"
+
+            config["model_attributes"]["name"] = config.pop("model")
+            config["model"] = config["model_attributes"]
+
+        # Calculate the edge indices on the fly
+        config["model"]["otf_graph"] = True
+
+        # Save config so obj can be transported over network (pkl)
+        self.config = copy.deepcopy(config)
+        self.config["checkpoint"] = checkpoint
+
+        if "normalizer" not in config:
+            del config["dataset"]["src"]
+            config["normalizer"] = config["dataset"]
+
+        super().__init__(
+            task=config["task"],
+            model=config["model"],
+            dataset=None,
+            optimizer=config["optim"],
+            identifier="",
+            normalizer=config["normalizer"],
+            slurm=config.get("slurm", {}),
+            local_rank=config.get("local_rank", 0),
+            is_debug=config.get("is_debug", True),
+            cpu=True,
+        )
+
+        if checkpoint is not None:
+            try:
+                self.load_checkpoint(checkpoint)
+            except NotImplementedError:
+                logging.warning("Unable to load checkpoint!")
+
+        self.a2g = AtomsToGraphs(
+            max_neigh=max_neighbors,
+            radius=cutoff,
+            r_energy=False,
+            r_forces=False,
+            r_distances=False,
+        )
+
+    def _backward(self, loss):
+        return super()._backward(loss)
+
+    def get_atoms_prediction(self, atoms):
+        data_object = self.a2g.convert(atoms)
+        batch = data_list_collater([data_object])
+        predictions = self.predict(
+            data_loader=batch, per_image=False, results_file=None, disable_tqdm=True
+        )
+        energy = predictions["energy"].item()
+        forces = predictions["forces"].cpu().numpy()
+        return energy, forces
