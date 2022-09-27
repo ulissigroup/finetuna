5776a022efc0bcfbbf735d515fc1077a05381f01
jmusiel <73840022+jmusiel@users.noreply.github.com>
2022-09-27 18:53:11 +0000

    remove outdated examples

--- examples/online_al_yilin_example/pt_emt_relaxation_example.py
+++
@@ -1,9 +0,0 @@
-import ase
-from finetuna.atomistic_methods import Relaxation
-from ase.optimize import BFGS
-from ase.calculators.emt import EMT
-
-initial_db = ase.io.read("Pt-init-images.db", ":")
-slab = initial_db[1]
-true_relax = Relaxation(slab, BFGS)
-true_relax.run(EMT(), "true_relax")
