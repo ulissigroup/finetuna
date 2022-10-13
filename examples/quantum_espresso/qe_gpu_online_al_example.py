c4dc868bb001943704f6b51004974cf45f3e1176
jmusiel <73840022+jmusiel@users.noreply.github.com>
2022-10-13 17:16:36 +0000

    move qe example to its own folder

--- examples/quantum_espresso/qe_gpu_online_al_example.py
+++ examples/quantum_espresso/qe_gpu_online_al_example.py
@@ -1,5 +1,5 @@
 # This is a script running Finetuna with GPU version of Quantum Espresso (QE).
-# This script uses QE build hosted on https://catalog.ngc.nvidia.com/orgs/hpc/containers/quantum_espresso, 
+# This script uses QE build hosted on https://catalog.ngc.nvidia.com/orgs/hpc/containers/quantum_espresso,
 # openmpi 4.1.2, gcc 11.1.0 and singularity 3.5.3 were used to start QE.
 # This script runs a relaxation for a system with CH3 on Cu 3*3*3 slab.
 
@@ -17,14 +17,14 @@ import os
 if __name__ == "__main__":
 
     traj = Trajectory(
-                "/work/westgroup/chao/qm_calc/new_cpox_dfts/adsorption/ch3_cu/oncv/ch3_cu_final.traj"
-           ) # change this path to your trajectory file
+        "/work/westgroup/chao/qm_calc/new_cpox_dfts/adsorption/ch3_cu/oncv/ch3_cu_final.traj"
+    )  # change this path to your trajectory file
 
     ml_potential = FinetunerEnsembleCalc(
         checkpoint_paths=[
             "/work/westgroup/chao/finetuna_models/s2ef/all/gemnet_t_direct_h512_all.pt",  # change this path to your gemnet checkpoint
         ],
-               mlp_params=[
+        mlp_params=[
             {
                 "tuner": {
                     "unfreeze_blocks": [
@@ -43,31 +43,26 @@ if __name__ == "__main__":
                     ],
                     "num_threads": 8,
                 },
-                
                 "trainer": "forces",
-                
-                "dataset":
-                    {
-                    "src": "/work/westgroup/opencatalyst/ocp/data/s2ef/all/train/", # change this to your database directory
+                "dataset": {
+                    "src": "/work/westgroup/opencatalyst/ocp/data/s2ef/all/train/",  # change this to your database directory
                     "normalize_labels": True,
                     "target_mean": -0.7554450631141663,
                     "target_std": 2.887317180633545,
                     "grad_target_mean": 0.0,
                     "grad_target_std": 2.887317180633545,
-                    },
-
+                },
                 "logger": "tensorboard",
-                
-                'task': {
-                    'dataset': 'trajectory_lmdb', 
-                    'description': 'Regressing to energies and forces for DFT trajectories from OCP', 
-                    'type': 'regression', 
-                    'metric': 'mae', 
-                    'labels': ['potential energy'], 
-                    'grad_input': 'atomic forces', 
-                    'train_on_free_atoms': True, 
-                    'eval_on_free_atoms': True},
-                    
+                "task": {
+                    "dataset": "trajectory_lmdb",
+                    "description": "Regressing to energies and forces for DFT trajectories from OCP",
+                    "type": "regression",
+                    "metric": "mae",
+                    "labels": ["potential energy"],
+                    "grad_input": "atomic forces",
+                    "train_on_free_atoms": True,
+                    "eval_on_free_atoms": True,
+                },
                 "model": {
                     "name": "gemnet_t",
                     "num_spherical": 7,
@@ -85,14 +80,14 @@ if __name__ == "__main__":
                     "num_atom": 3,
                     "cutoff": 6.0,
                     "max_neighbors": 50,
-                    "rbf":{
+                    "rbf": {
                         "name": "gaussian",
                     },
-                    "envelope":{
+                    "envelope": {
                         "name": "polynomial",
                         "exponent": 5,
                     },
-                    "cbf":{
+                    "cbf": {
                         "name": "spherical_harmonics",
                     },
                     "extensive": True,
@@ -108,7 +103,7 @@ if __name__ == "__main__":
                     "eval_batch_size": 32,
                     "eval_every": 5000,
                     "num_workers": 2,
-                    "lr_initial": 5.e-4,
+                    "lr_initial": 5.0e-4,
                     "optimizer": "AdamW",
                     "optimizer_params": {"amsgrad": True},
                     "scheduler": "ReduceLROnPlateau",
@@ -126,39 +121,39 @@ if __name__ == "__main__":
             },
         ],
     )
-    
+
     # change this to your QE settings
     espresso_settings = {
-        'control': {
-            'verbosity': 'high',
-            'calculation': 'scf',
+        "control": {
+            "verbosity": "high",
+            "calculation": "scf",
         },
-        'system': {
-            'input_dft': 'BEEF-VDW',
-            'occupations': 'smearing',
-            'smearing': 'mv',
-            'degauss': 0.01,
-            'ecutwfc': 60,
+        "system": {
+            "input_dft": "BEEF-VDW",
+            "occupations": "smearing",
+            "smearing": "mv",
+            "degauss": 0.01,
+            "ecutwfc": 60,
         },
-        'electrons': {
-            'electron_maxstep': 200,
-            'mixing_mode': 'local-TF',
-            'conv_thr': 1e-8,
+        "electrons": {
+            "electron_maxstep": 200,
+            "mixing_mode": "local-TF",
+            "conv_thr": 1e-8,
         },
     }
 
     pseudopotentials = {
-        'Cu': 'Cu_ONCV_PBE-1.2.upf',
-        'C': 'C_ONCV_PBE-1.2.upf',
-        'O': 'O_ONCV_PBE-1.2.upf',
-        'H': 'H_ONCV_PBE-1.2.upf',
+        "Cu": "Cu_ONCV_PBE-1.2.upf",
+        "C": "C_ONCV_PBE-1.2.upf",
+        "O": "O_ONCV_PBE-1.2.upf",
+        "H": "H_ONCV_PBE-1.2.upf",
     }
 
-    unixsocket = 'unix'
+    unixsocket = "unix"
 
     PWD = os.getcwd()
 
-    gpu_num = 4 # change this to match the number of gpus on your machine
+    gpu_num = 4  # change this to match the number of gpus on your machine
 
     command = f"singularity run --nv -B{PWD}:/host_pwd --pwd /host_pwd docker://nvcr.io/hpc/quantum_espresso:qe-7.0 mpirun -np {gpu_num} pw.x -in espresso.pwi --ipi {unixsocket}:UNIX > espresso.pwo"
 
@@ -171,7 +166,9 @@ if __name__ == "__main__":
         input_data=espresso_settings,
     )
 
-    with SocketIOCalculator(espresso, log=sys.stdout, unixsocket=unixsocket) as parent_calc:
+    with SocketIOCalculator(
+        espresso, log=sys.stdout, unixsocket=unixsocket
+    ) as parent_calc:
         learner = OnlineLearner(
             learner_params={
                 "stat_uncertain_tol": 1000000,
