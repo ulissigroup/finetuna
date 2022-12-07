1999db8eaa73fa06f65927907f0c146ba2f1d06d
LoryWang <xiaoxia3@andrew.cmu.edu>
2022-12-07 21:55:43 +0000

    update wrapper so that potcar is not necessary, vaspinteractive relaxation can be called, and not changing file names if path is provided

--- finetuna/vasp_wrapper/finetuna_wrap.py
+++ finetuna/vasp_wrapper/finetuna_wrap.py
@@ -25,16 +25,25 @@ def main(args):
     kpoints = Kpoints.from_file(os.path.join(args.path, "KPOINTS"))
     vasp_inputs = {
         "kpts": kpoints.kpts[0],
+        "xc": args.xc,
     }
     for k in incar.keys():
         vasp_inputs[k.lower()] = incar[k]
 
-    os.rename(
-        os.path.join(args.path, "INCAR"), os.path.join(args.path, "INCAR_original")
-    )
-    os.rename(
-        os.path.join(args.path, "KPOINTS"), os.path.join(args.path, "KPOINTS_original")
-    )
+    # Read the initial structure
+    initial_structure = ase.io.read(os.path.join(args.path, "POSCAR"))
+    if args.path == "":
+        os.rename(
+            os.path.join(args.path, "POSCAR"),
+            os.path.join(args.path, "POSCAR_original"),
+        )
+        os.rename(
+            os.path.join(args.path, "INCAR"), os.path.join(args.path, "INCAR_original")
+        )
+        os.rename(
+            os.path.join(args.path, "KPOINTS"),
+            os.path.join(args.path, "KPOINTS_original"),
+        )
 
     # Set convergence criteria as EDIFFG in VASP flag, default to 0.03 eV/A
     if -vasp_interactive.exp_params.get("ediffg") == 0:
@@ -42,69 +51,80 @@ def main(args):
     else:
         relax_fmax = -vasp_interactive.exp_params.get("ediffg")
 
-    # Read the initial structure
-    initial_structure = ase.io.read(os.path.join(args.path, "POSCAR"))
-    os.rename(
-        os.path.join(args.path, "POSCAR"), os.path.join(args.path, "POSCAR_original")
-    )
-
     # Parse the config file
     yaml_file = open(args.config)
     # Set VASP command
     parsed_yaml_file = yaml.load(yaml_file, Loader=yaml.FullLoader)
-    # Set up learner, finetuner
-    learner_params = parsed_yaml_file["learner"]
-    learner_params["fmax_verify_threshold"] = relax_fmax
 
-    finetuner = parsed_yaml_file["finetuner"]
-    optional_config = parsed_yaml_file.get("optional_config", None)
-    # Set up Finetuner calculator
-    print("------Setting up Finetuner Calculator------")
-    ml_potential = FinetunerEnsembleCalc(
-        checkpoint_paths=[args.checkpoint],
-        mlp_params=finetuner,
-    )
+    if not args.vasponly:
+        # Set up learner, finetuner
+        learner_params = parsed_yaml_file["learner"]
+        learner_params["fmax_verify_threshold"] = relax_fmax
+
+        finetuner = parsed_yaml_file["finetuner"]
+        optional_config = parsed_yaml_file.get("optional_config", None)
+        # Set up Finetuner calculator
+        print("------Setting up Finetuner Calculator------")
+        ml_potential = FinetunerEnsembleCalc(
+            checkpoint_paths=[args.checkpoint],
+            mlp_params=finetuner,
+        )
+
     with VaspInteractive(**vasp_inputs) as parent_calc:
-        parent_calc.read_potcar(filename=os.path.join(args.path, "POTCAR"))
-        os.rename(
-            os.path.join(args.path, "POTCAR"),
-            os.path.join(args.path, "POTCAR_original"),
-        )
+        if args.vasponly:
+            initial_structure.calc = parent_calc
+            dyn = BFGS(initial_structure, trajectory="vasp_inter_bfgs_relax.traj")
+            print(
+                f"------Starting Relaxation. Terminate when Fmax <{relax_fmax} eV/A------"
+            )
+            dyn.run(
+                fmax=relax_fmax,
+            )
+            print("------Relaxation Ends------")
 
-        onlinecalc = OnlineLearner(
-            learner_params,
-            [],
-            ml_potential,
-            parent_calc,
-            optional_config=optional_config,
-        )
-        initial_structure.calc = onlinecalc
-        dyn = BFGS(
-            initial_structure,
-            trajectory=parsed_yaml_file["relaxation"].get("trajname", "online_al.traj"),
-            maxstep=parsed_yaml_file["relaxation"].get("maxstep", None),
-        )
-        dyn.attach(parent_only_replay, 1, initial_structure.calc, dyn)
+        else:
+            onlinecalc = OnlineLearner(
+                learner_params,
+                [],
+                ml_potential,
+                parent_calc,
+                optional_config=optional_config,
+            )
+            initial_structure.calc = onlinecalc
+            dyn = BFGS(
+                initial_structure,
+                trajectory=parsed_yaml_file["relaxation"].get(
+                    "trajname", "finetuna.traj"
+                ),
+                maxstep=parsed_yaml_file["relaxation"].get("maxstep", None),
+            )
+            dyn.attach(parent_only_replay, 1, initial_structure.calc, dyn)
 
-        print(
-            f"------Starting Relaxation. Terminate when Fmax <{relax_fmax} eV/A------"
-        )
-        dyn.run(
-            fmax=relax_fmax,
-        )
-        print("------Relaxation Ends------")
-        print(
-            f"------{onlinecalc.parent_calls} VASP Single Point Calculations Required------"
-        )
+            print(
+                f"------Starting Relaxation. Terminate when Fmax <{relax_fmax} eV/A------"
+            )
+            dyn.run(
+                fmax=relax_fmax,
+            )
+            print("------Relaxation Ends------")
+            print(
+                f"------{onlinecalc.parent_calls} VASP Single Point Calculations Required------"
+            )
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="VASP Input Wrapper for Finetuna")
     parser.add_argument(
+        "-xc",
+        "--xc",
+        type=str,
+        required=True,
+        help="Exchange-correlation functional for VASP, e.g.: pbe, rpbe, beef-vdw. Check https://wiki.fysik.dtu.dk/ase/ase/calculators/vasp.html#exchange-correlation-functionals for the keys.",
+    )
+    parser.add_argument(
         "-c",
         "--checkpoint",
         type=str,
-        required=True,
         help="Path to the checkpoint",
     )
     parser.add_argument(
@@ -121,5 +141,14 @@ if __name__ == "__main__":
         default=files("finetuna.vasp_wrapper").joinpath("sample_config.yml"),
         help="Path to the config",
     )
+    parser.add_argument(
+        "-vasponly",
+        "--vasponly",
+        action="store_true",
+        help="Run a VASP Interactive relaxation with BFGS, no FineTuna!",
+    )
     args = parser.parse_args()
+    print(args.vasponly is False)
+    if args.vasponly is False and args.checkpoint is None:
+        parser.error("Please provide a path to the checkpoint for using FineTuna.")
     main(args)
